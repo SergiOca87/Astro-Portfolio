5e9f8d5419f8f14b40cc408090c8fafcc1ab0450
SergiOca87 <ocasergi@gmail.com>
2023-12-06 21:27:55 +0100

    card update

---
+++ data/projects.js
@@ -0,0 +1,60 @@
+const projects = [
+    {
+        title: 'Knowledge Portfolio',
+        description: 'A Full Stack NextJS App',
+        stackList: 'Next.js 12, Supabase, Styled Components',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Open AI API',
+        description: 'A small App where I use the OpenAI API',
+        stackList: 'Next.js 12, Supabase, Styled Components',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Algorithms',
+        description: 'An algorithm blog where I keep my solutions to FreecodeCamp or Codewars problems',
+        stackList: 'React, Javascript',
+        img: '../algorithms.jpg',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Gatsby JS Project',
+        description: 'A GatsbyJS website using WordPress as data source',
+        stackList: 'Next.js 12, Supabase, Styled Components',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'CS50 Capstone web',
+        description: "A Python fullstack project for Hardvard's CS50 course",
+        stackList: 'Next.js 12, Supabase, Styled Components',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Library',
+        description: "An app that's part of the Odin Project curriculum",
+        stackList: 'WordPress, Javascript, CSS, SoftSkills',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Monsterslayer',
+        description: 'A VueJS toy app to play and learn some Vue',
+        stackList: 'Next.js 12, Supabase, Styled Components',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    },
+    {
+        title: 'Professional Work',
+        description: 'A description of what I do for a living',
+        stackList: 'WordPress, Javascript, CSS, SoftSkills',
+        img: '../algorithms.png',
+        stackBgColor: 'bg-cyan-400'
+    }
+];
+
+export default projects;
